f66b82f3d6cd3a6471bf35ca1e72d38fee48a46f
Maliksncf <abdelmalik-el-kaddous.bougherara@sncf.fr>
2022-06-03 15:24:37 +0200

    ola

---
+++ assets/js/class/ElementHTML.js
@@ -0,0 +1,10 @@
+class ElementHTML{
+    
+    header(options){
+        const option = {};
+        options.forEach(( key, value )=>{
+            console.log(key, value);
+        })
+    }
+}
+export default ElementHTML;
---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+import elm from './class/ElementHTML.js';
+const test = new elm();
+test.header("id:monid", "class:maclass");
